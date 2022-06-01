cb1d6e006b9057b43b9238775812c0b5d491de88
Aoi Yukimura <dudestin8@gmail.com>
2022-06-01 11:45:13 +0900

    add lua scripts

---
+++ src/nyancat.lua
@@ -0,0 +1,31 @@
+local monitor = peripheral.find("monitor")
+monitor.clear()
+require('animation')
+
+local palette = {}
+palette[',']  = colors.blue  -- Blue background */
+palette['.']  = colors.white -- White stars */
+palette['\''] = colors.black  -- Black border */
+palette['@']  = colors.brown -- Tan poptart */
+palette['$']  = colors.pink -- Pink poptart */
+palette['-']  = colors.red -- Red poptart */
+palette['>']  = colors.red -- Red rainbow */
+palette['&']  = colors.orange -- Orange rainbow */
+palette['+']  = colors.yellow -- Yellow Rainbow */
+palette['#']  = colors.green -- Green rainbow */
+palette['=']  = colors.lightBlue  -- Light blue rainbow */
+palette[';']  = colors.blue  -- Dark blue rainbow */
+palette['*']  = colors.gray -- Gray cat face */
+palette['%']  = colors.pink -- Pink cheeks */
+
+local old_monitor = term.redirect(monitor)
+for i, frame in ipairs(frames) do
+    for x, row_str in ipairs(frame) do
+        y = 1
+        for pixel_char, #row_str do
+            paintutils.drawPixel(x, y, palette[pixel_char]) 
+            y += 1
+        end
+    end
+end
+term.redirect(old_monitor)
